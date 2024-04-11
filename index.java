7d39831a28e91d907b286bd6608b6e7c4e7c22bf
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2024-04-11 09:16:55 -0400

    Update index.java

--- index.java
+++ index.java
@@ -31,19 +31,19 @@ final class Main {
         fruits.push("apple");
         fruits.push("banana");
         fruits.push("cherry");
-        System.out.println("fruits: " + fruits.showStack());
+        System.out.println("fruits: " + fruits.show());
 
         final CarStack colours = new CarStack();
         colours.push("red");
         colours.push("green");
         colours.push("blue");
-        System.out.println("colours: " + colours.showStack());
+        System.out.println("colours: " + colours.show());
 
         final CarStack cars = new CarStack();
-        cars.push("square");
-        cars.push("triangle");
-        cars.push("circle");
-        System.out.println("cars: " + cars.showStack());
+        cars.push("Toyota");
+        cars.push("Nissan");
+        cars.push("Mazda");
+        System.out.println("cars: " + cars.show());
 
         System.out.println("\nDone.");
     }
